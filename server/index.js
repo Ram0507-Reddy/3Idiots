6b6f5043207802148f4f0e00e522f16c266c9783
Ram0507-Reddy <shriramreddyofficial0507@gmail.com>
2026-01-22 02:30:24 +0530

    Initial commit of 3IDIOTS - Full Stack Academic Platform

---
+++ server/index.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// Load env vars
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Database Connection
+require('./config/db');
+
+// Routes
+app.use('/api', require('./routes/api'));
+
+app.get('/', (req, res) => {
+    res.send('Uninest API is running...');
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
